1dc4b5510412dcd27c270f469377498fc0806ade
gustavo.olliveira49@gmail.com <gustavo.olliveira49@gmail.com>
2025-04-24 21:57:39 -0300

    Iniciando estudos de react

---
+++ react-studies/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Introduction } from "@/components/introduction/Introduction";
+
+const Home = () => {
+  return (
+    <>
+      <Introduction />
+    </>
+  );
+}
+
+export default Home;
+
---
+++ react-studies/src/components/introduction/HelloWorld.tsx
@@ -0,0 +1,7 @@
+export const HelloWorld = () => {
+    return(
+        <>
+            <h1 className="w-40 m-5 font-bold text-2xl text-green-300 text-center">Olá, Mundo</h1>
+        </>
+    );
+}
